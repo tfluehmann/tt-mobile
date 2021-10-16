d0b34f07000d6d6918b310dd73864fde8cd272bd
tobias <tobias@fluehmann.one>
2021-10-16 23:01:12 +0200

    working docker-compose setup

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,6 +3,8 @@ require("dotenv").config({ path: "../.env"});
 module.exports = {
     client: "mysql",
     connection: {
+      host: process.env.DB_HOST || "localhost",
+      port: process.env.DB_PORT || 3306,
       database: process.env.DB_NAME || "db",
       user: process.env.DB_USER || "ttmobile",
       password: process.env.DB_PASSWORD || "ttmobile",
